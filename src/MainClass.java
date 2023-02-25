4ad47bc49c27264403d4c5a298afce1d9fbc6071
borodinact <borodinact@yandex.ru>
2023-02-25 17:15:29 +0300

    add testGetClassNumber

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,15 +1,8 @@
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 public class MainClass {
 
-    public int getLocalNumber() {
-        return 14;
-    }
-    @Test
-    public void testGetLocalNumber()
-    {
-        assertTrue(getLocalNumber() == 14, "При вызове метода getLocalNumber не пришло число 14");
+    private final int class_number = 20;
+
+    public int getClassNumber() {
+        return this.class_number;
     }
 }
---
+++ src/MainClassTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainClassTest {
+    @Test
+    public void testGetClassNumber() {
+        MainClass mainClass = new MainClass();
+        assertTrue(mainClass.getClassNumber() > 45,
+                "Метод getClassNumber вернул число не больше 45");
+    }
+}
