152a55ce487e4544801698b17fc7587ef2e16918
borodinact <borodinact@yandex.ru>
2023-02-25 18:13:36 +0300

    add testGetClassString()

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,8 +1,11 @@
 public class MainClass {
-
+    private final String class_string = "Hello, world";
     private final int class_number = 20;
 
     public int getClassNumber() {
         return this.class_number;
+    }
+    public String getClassString() {
+        return this.class_string;
     }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -5,6 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class MainClassTest {
 
     @Test
+    public void testGetClassString() {
+        MainClass mainClass = new MainClass();
+        assertTrue(mainClass.getClassString().matches(".*[Hh]ello.*"),
+                "метод getClassString не вернул строку, в которой есть подстрока “hello” или “Hello");
+    }
+    @Test
     public void testGetClassNumber() {
         MainClass mainClass = new MainClass();
         assertTrue(mainClass.getClassNumber() > 45,
