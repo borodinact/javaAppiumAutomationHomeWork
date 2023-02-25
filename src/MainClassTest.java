eeca60b30353a64cd4df1226586b21b6486b4ad2
borodinact <borodinact@yandex.ru>
2023-02-25 18:12:37 +0300

    ...

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -3,12 +3,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainClassTest {
-    @Test
-    public void testGetClassString() {
-        MainClass mainClass = new MainClass();
-        assertTrue(mainClass.getClassString().matches(".*[Hh]ello.*"),
-                "метод getClassString не вернул строку, в которой есть подстрока “hello” или “Hello");
-    }
 
     @Test
     public void testGetClassNumber() {
