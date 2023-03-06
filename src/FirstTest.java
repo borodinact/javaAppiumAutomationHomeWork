86a8656234f9c146f08112e8b90fad68fc2509b6
borodinact <borodinact@yandex.ru>
2023-03-07 00:30:03 +0300

    testSearchFieldText - тест проверяет, что поле ввода для поиска статьи содержит текст assertElementHasText - функция, которая проверяет наличие ожидаемого текста у элемента

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -31,8 +36,30 @@ public class FirstTest {
         driver.quit();
     }
 
+
+    /**
+     * тест проверяет, что поле ввода для поиска статьи содержит текст
+     */
     @Test
-    public void firstTest() {
-        System.out.println("First test run");
+    public void testSearchFieldText() {
+        assertElementHasText(
+                By.className("android.widget.TextView"),
+                "Поиск по Википедии",
+                "Cannot find expected text 'Поиск по Википедии'"
+                );
+    }
+
+    /**
+     * функция, которая проверяет наличие ожидаемого текста у элемента
+     */
+    private void assertElementHasText(By by, String expectedText, String errorMessage){
+        String actualText= waitForElementPresent(by).getText();
+        Assert.assertEquals(errorMessage, expectedText, actualText);
+    }
+
+    private WebElement waitForElementPresent(By by){
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.withMessage("Cannot find webElement by locator " + by);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
 }
